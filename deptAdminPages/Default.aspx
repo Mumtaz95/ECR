dfe1e8fafec56ce585d172e22cfea18345ee2ec9
Mumtaz Ahmed Siddiqui <mumtaz.sid995@gmail.com>
2017-11-21 13:25:01 +0500

    Add files via upload

---
+++ deptAdminPages/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="deptAdminPages_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server" DataKeyNames="firID" OnRowCancelingEdit="GridView1_RowCancelingEdit"
+             OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" OnPageIndexChanging="GridView1_PageIndexChanging" AutoGenerateColumns="False">
+            <Columns>
+                <asp:TemplateField HeaderText="ID" SortExpression="firID">
+                    <EditItemTemplate>
+                        <%--<asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("firID") %>'></asp:TextBox>--%>
+                        <asp:Label ID="Label1" runat="server" Text='<%# Bind("firID") %>'></asp:Label>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label1" runat="server" Text='<%# Bind("firID") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Complain" SortExpression="Complainant">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox2" runat="server" Text='<%# Bind("Complainant") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label2" runat="server" Text='<%# Bind("Complainant") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:CommandField HeaderText="Action" ShowDeleteButton="True" ShowEditButton="True" ShowHeader="True" />
+            </Columns>
+
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ deptAdminPages/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LoginSignUp;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class deptAdminPages_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            LoadGrid();
+        }
+    }
+
+    private void LoadGrid()
+    {
+        SqlConnection con = ConnectionString.Connect();
+        con.Open();
+        SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM FIR",con);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+        con.Close();
+    }
+
+    protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        LoadGrid();
+    }
+
+    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        LoadGrid();
+    }
+
+    protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        int userid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+        GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+        //TextBox textName = (TextBox)GridView1.Rows[e.RowIndex].FindControl("Complainant"); //row.FindControl("Complainant");
+        //TextBox textName = (TextBox)GridView1.Rows[e.RowIndex].FindControl("Complainant");
+        //if(string.IsNullOrWhiteSpace(textName.Text))
+        //{
+        //    textName.Text = "bestbest@gmail.com";
+        //}
+        string textName = "bestbest@gmail.com";
+
+        SqlConnection con = ConnectionString.Connect();
+        con.Open();
+        SqlCommand cmd = new SqlCommand("update FIR set Complainant = '"+textName+ "' where firID = '"+userid+"'", con);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        GridView1.EditIndex = -1;
+        LoadGrid();
+
+    }
+
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        int userid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+        SqlConnection con = ConnectionString.Connect();
+        con.Open();
+        SqlCommand cmd = new SqlCommand("delete from FIR where firID = '" + userid + "'", con);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        LoadGrid();
+    }
+
+    protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        LoadGrid();
+    }
+}
